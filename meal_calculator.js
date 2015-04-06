840377c4b55cb207d00ea439f7bff7e7802dde3c
Justin Hill <justinmhill@gmail.com>
2015-04-05 23:43:32 -0400

    Move calculating functions into a cash register object. All calculating functions are methods of cashRegister

--- meal_calculator.js
+++ meal_calculator.js
@@ -5,12 +5,10 @@
 
 
 // globals
-var dinerList = [];
 var taxRate = 0.06;
 var tipRate = 0.20;
 
 // Diner object constructor
-
 
 var Diner = function(name) {
   this.name = name;
@@ -28,82 +26,69 @@ Diner.prototype.addDish = function(name, price) {
   );
 };
 
-// Add a new diner to the list of people dining.
-
-function createDiner(name) {
-  var newDiner = new Diner(name);
-  dinerList.push(newDiner);
-  return newDiner;
-};
-
-// Calculate the bill of an individual diner, *including tax.*
-
-function calculateDinerBill(diner) {
-  var dinerBill = 0;
-  for (var i = 0; i < diner.dishes.length; i++) {
-    dinerBill += diner.dishes[i]["price"];
-  }
-  return addTax(dinerBill, taxRate);
+var cashRegister = {
+  dinerList: [],
+  // Add a new diner to the list of people dining.
+  createDiner: function (name) {
+    var newDiner = new Diner(name);
+    cashRegister.dinerList.push(newDiner);
+    return newDiner;
+  },
+  // Calculate the bill of an individual diner, *including tax.*
+  calculateDinerBill: function (diner) {
+    var dinerBill = 0;
+    for (var i = 0; i < diner.dishes.length; i++) {
+      dinerBill += diner.dishes[i]["price"];
+    }
+    return cashRegister.addTax(dinerBill, taxRate);
+  },
+  // Total up cost of all diner's meals (tax included previously).
+  calculateTotalBill: function (diners) {
+    var total = 0;
+    for (var i = 0; i < diners.length; i++) {
+      total += cashRegister.calculateDinerBill(diners[i]);
+    }
+    return total;
+  },
+  // Add a fixed tax percentage to a bill
+  addTax: function (bill, taxRate) {
+    return bill + (bill * taxRate);
+  },
+  // Add a percentage tip to the total bill
+  calculateTip: function (totalBill, tipRate) {
+    return totalBill * tipRate
+  },
+  // Print out the total bill for the table.
+  printGrandTotal: function () {
+    var dinerList = cashRegister.dinerList;
+    var base = cashRegister.calculateTotalBill(dinerList);
+    var tip = cashRegister.calculateTip(base, tipRate);
+    var total = base + tip;
+    console.log("The grand total for the table is $" + total.toFixed(2) + ".");
+  },
+  // Print a breakdown of what each diner owes.
+  printBreakdown: function () {
+    var dinerList = cashRegister.dinerList;
+    var tip = cashRegister.calculateTip(cashRegister.calculateTotalBill(dinerList), tipRate);
+    var tipPerPerson = (tip / dinerList.length);
+    var amountOwedMessage = "";
+      for (var i=0; i < dinerList.length; i++) {
+        var base = cashRegister.calculateDinerBill(dinerList[i]);
+        var amountOwed = base + tipPerPerson;
+        amountOwedMessage += dinerList[i].name + " owes $" + amountOwed.toFixed(2) + ".\n"; 
+      }
+    console.log(amountOwedMessage);
+  } 
 }
 
-// Total up cost of all diner's meals (tax included previously).
-
-function calculateTotalBill(diners) {
-  var total = 0;
-  for (var i = 0; i < diners.length; i++) {
-    total += calculateDinerBill(diners[i]);
-  }
-  return total;
-};
-
-// Add a fixed tax percentage to a bill
-
-function addTax(bill, taxRate) {
-  return bill + (bill * taxRate);
-}
-
-// Add a percentage tip to the total bill
-
-function calculateTip(totalBill, tipRate) {
-  return totalBill * tipRate
-}
-
-
-
-
-// Print out the total bill for the table.
-
-function printGrandTotal() {
-  var base = calculateTotalBill(dinerList);
-  var tip = calculateTip(base, tipRate);
-  var total = base + tip;
-  console.log("The grand total for the table is $" + total.toFixed(2) + ".");
-}
-
-// Print a breakdown of what each diner owes.
-
-function printBreakdown() {
-  var tip = calculateTip(calculateTotalBill(dinerList), tipRate);
-  var tipPerPerson = (tip / dinerList.length);
-  var amountOwedMessage = "";
-
-  for (var i=0; i < dinerList.length; i++) {
-    var base = calculateDinerBill(dinerList[i]);
-    var amountOwed = base + tipPerPerson;
-    amountOwedMessage += dinerList[i].name + " owes $" + amountOwed.toFixed(2) + ".\n"; 
-  }
-  console.log(amountOwedMessage);
-}
 
 // TEST
-var justin = createDiner("Justin");
-var maggie = createDiner("Maggie");
-var fig = createDiner("Fig")
+var justin = cashRegister.createDiner("Justin");
+var maggie = cashRegister.createDiner("Maggie");
 justin.addDish("Pizza", 13.95);
 justin.addDish("Beer", 3.95);
 maggie.addDish("Burger", 7.95);
 maggie.addDish("Wine", 4.95);
-fig.addDish("Salmon", 28.00);
 
-printGrandTotal();
-printBreakdown();
+cashRegister.printGrandTotal();
+cashRegister.printBreakdown();
